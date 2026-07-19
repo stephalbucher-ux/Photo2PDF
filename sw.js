461609a6d04f80ae79edc863c9c9afaf44a19b1f
stephalbucher-ux <steph.albucher@gmail.com>
2026-07-19 14:49:04 +0200

    Auto-héberger OpenCV.js (CDN docs.opencv.org indisponible)

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
 /* Scanorama service worker — offline support */
-const CACHE = "scanorama-v1";
+const CACHE = "scanorama-v2";
 const SHELL = [
   "./",
   "./index.html",
   "./style.css",
   "./app.js",
+  "./opencv.js",
   "./manifest.webmanifest",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
